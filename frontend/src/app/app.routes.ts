c6405f262ff55d617269244803905e986850a5aa
bilalshahzadd <bilalshahzad521@gmail.com>
2024-07-11 07:23:12 +0500

    signup page complete

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TodosComponent } from './pages/todos/todos.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "task-list", component: TodosComponent}
+    {path: "task-list", component: TodosComponent},
+    {path: "sign-up", component: SignupComponent},
+    {path: "login", component: LoginComponent}
 ];
---
+++ frontend/src/app/pages/signup/signup.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  signupData: any = {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    gender: ''
+  }
+
+}
